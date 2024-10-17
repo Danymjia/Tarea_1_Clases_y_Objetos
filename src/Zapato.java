5d4857c2069b3ccaa45830f8fc5766e84209e4a4
Dany Mejía <168014765+Danymjia@users.noreply.github.com>
2024-10-17 17:18:05 -0500

    Deber 1 Clases y Objetos

---
+++ src/Zapato.java
@@ -0,0 +1,44 @@
+public class Zapato {
+
+    float tallaZapato;
+    String marcaZapato;
+    String colorZapato;
+
+    public Zapato(float tallaZapato, String marcaZapato, String colorZapato ) {
+        this.tallaZapato = tallaZapato;
+        this.marcaZapato = marcaZapato;
+        this.colorZapato = colorZapato;
+    }
+
+    public float getTallaZapato() {
+        return tallaZapato;
+    }
+
+    public void setTallaZapato(float tallaZapato) {
+        this.tallaZapato = tallaZapato;
+    }
+
+    public String getMarcaZapato() {
+        return marcaZapato;
+    }
+
+    public void setMarcaZapato(String marcaZapato) {
+        this.marcaZapato = marcaZapato;
+    }
+
+    public String getColorZapato() {
+        return colorZapato;
+    }
+
+    public void setColorZapato(String colorZapato) {
+        this.colorZapato = colorZapato;
+    }
+
+    public void mostrarZapato() {
+        System.out.println("--------Zapato--------");
+        System.out.println("Talla Zapato: " + getTallaZapato());
+        System.out.println("Marca Zapato: " + getMarcaZapato());
+        System.out.println("Color: " + getColorZapato());
+    }
+}
+
